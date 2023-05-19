d4e0808e0a0b86ecfc7e969ada52874b4d4062b8
Cameron Hall <cameron.hall@fnb.co.za>
2023-05-19 07:42:03 +0200

    update metadata version

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description       'Installs the iptables daemon and provides resources for manag
 source_url        'https://github.com/chef-cookbooks/iptables'
 issues_url        'https://github.com/chef-cookbooks/iptables/issues'
 chef_version      '>= 15.3'
-version           '8.0.0'
+version           '8.0.1'
 
 %w(redhat centos debian ubuntu amazon scientific oracle amazon zlinux).each do |os|
   supports os
